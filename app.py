58567db3648b4b85f9fd40f3b18632ea4355b66c
Mark <limiter_inksk.0k@icloud.com>
2025-07-11 12:30:12 +0100

    got the app working again after exoframe issues with the Makers account

--- app.py
+++ app.py
@@ -10,5 +10,6 @@ def get_home():
 if __name__ == '__main__':
     app.run(
       debug=True,
-      host="0.0.0.0" # Listen for connections _to_ any server
+      host="0.0.0.0", # Listen for connections _to_ any server
+      port=5001
     )
